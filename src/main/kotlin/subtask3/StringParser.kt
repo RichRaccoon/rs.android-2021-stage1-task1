964b529ad4a6c5e2b5e0e94c6d3b1ebb1931311f
Elias <thomas.cat.thomas@gmail.com>
2021-05-31 17:21:36 +0300

    feat: implement Task3 - StringParser

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,41 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val result: MutableList<String> = mutableListOf()
+        var tempString: String
+        var openingChar: Char
+        var closingChar: Char
+        var lastIndex = 0
+        var countChar = 0
+
+        for (i in inputString.indices) {
+            if (inputString[i] == '[' || inputString[i] == '<' || inputString[i] == '(') {
+                openingChar = inputString[i]
+                closingChar = if (openingChar == '(') inputString[i] + 1 else inputString[i] + 2
+
+                tempString = inputString.substring(i + 1)
+
+                for (j in tempString.indices) {
+                    if (tempString[j] == openingChar) {
+                        countChar += 1
+                    }
+                }
+
+                for (j in tempString.indices) {
+                    if (tempString[j] == closingChar) {
+                        countChar -= 1
+                        if (countChar == 0 || countChar == -1) {
+                            lastIndex = j
+                            countChar = 0
+                            break
+                        }
+                    }
+                }
+
+                result.add(tempString.substring(0, lastIndex))
+            }
+        }
+
+        return result.toTypedArray()
     }
 }
